092c735d65769892dd5f8f229fda0fee49dd8317
Kinremtus <kinremtus@example.com>
2026-04-05 12:38:53 +0300

    feat: add CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from database import engine, Base
 from routers import auth, media, tracking, search
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="MediaTracker")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",  # локальная разработка
+        "https://mediatracker.web-socket-test-bench.site:2053",  # прод
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(media.router)
