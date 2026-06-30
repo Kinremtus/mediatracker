35565549a36c4fabef5492e706b950a9356f5cc9
Kinremtus <kinremtus@example.com>
2026-06-30 12:57:00 +0300

    tf: manage mediatracker CNAME via Cloudflare Tunnel

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "main" {
   zone_id = var.cloudflare_zone_id
-  name    = "@"
-  content = var.server_ip
-  type    = "A"
+  name    = "mediatracker"
+  type    = "CNAME"
+  content = "0f6eebb2-ff0e-49fc-92da-3379ebc12cc1.cfargotunnel.com"
   proxied = true
 }
